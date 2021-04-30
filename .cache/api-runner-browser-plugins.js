5b1476af70f639293b95302daf042b0baaf98995
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2021-04-30 01:23:34 +0100

    chore: Deploy tofumatt.com

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"Matthew Riley MacPherson is on the Internet.","short_name":"Matthew Riley MacPherson is on the Internet.","start_url":"","background_color":"#ffffff","theme_color":"#25303B","display":"minimal-ui","cache_busting_mode":"query","include_favicon":true,"legacy":true,"theme_color_in_head":true,"cacheDigest":null},
+    },{
+      plugin: require('../gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
